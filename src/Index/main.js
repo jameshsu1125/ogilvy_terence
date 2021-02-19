4deb4111a5fba9e1f60e2bb2594430bba3961dbe
James Hsu <70932507+jameshsu1125@users.noreply.github.com>
2021-02-19 20:11:55 +0800

    Initial commit

---
+++ src/Index/main.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './main.scss';
+
+export default class index extends React.Component {
+	constructor(props) {
+		super(props);
+		const root = this;
+		//script
+	}
+
+	componentDidMount() {
+		//script
+	}
+
+	componentDidUpdate() {
+		//script
+	}
+
+	componentWillUnmount() {
+		//script
+	}
+
+	render() {
+		return <div id='index'></div>;
+	}
+}
